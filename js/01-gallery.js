9654b760bc8400b430c13280d48a6ff039c05b1e
Oleksandr <avo_sas@ukr.net>
2023-01-07 19:54:54 +0200

    01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const createGallery = document.querySelector('.gallery')
+const cards = createItem(galleryItems);
+  
+createGallery.insertAdjacentHTML('beforeend', cards)
 
-console.log(galleryItems);
+function createItem(cards) {
+    return cards.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                <img class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+                </a>
+            </div>`}).join('');
+};
+
+const onGalleryClick = (evt) => {
+    evt.preventDefault();
+    if (evt.target.classList.contains("gallery")) return;
+    const source = evt.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+    instance.show();
+
+    window.addEventListener("keydown", (event) => {
+        if (event.key === "Escape") {
+            instance.close();
+        };
+    }, { once: true })
+};
+
+
+createGallery.addEventListener('click', onGalleryClick)
+
+//console.log(galleryItems);
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -8,6 +8,13 @@ export const galleryItems = [
   },
   {
     preview:
+      'https://pixabay.com/get/g73271ad84af8d29b09a30ac40f464d40514b826ac111b7d4856e9334eb7dc994cd8fdaa55dde4333b1b587a86a32463b_640.jpg',
+    original:
+      'https://pixabay.com/get/gf7ff427605146b1358f260fb4e68baa9cd725c99a0b8438effa89b745beac037c31ab619813e33b84b051b523fa45fdd462c1f9b2357ff4767cf1b6af0b293f4_1280.jpg',
+    description: 'Stop The War In Ukraine',
+  },
+  {
+    preview:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
     original:
       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
@@ -61,5 +68,19 @@ export const galleryItems = [
     original:
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/gc0060cc621bbce67b0ae1cef310185c806675be38299e9adca53c7005a4b94b0ce55f56477101318bea8d098ffd89bfb_640.jpg',
+    original:
+      'https://pixabay.com/get/g99ca9192942b12444b8731424b34d76d856bba25c32d89934aa24cb9172bd61b383a0e94e768be747368f887aa667a78c8cddc770f1445f7a96d59ce242921b4_1920.jpg',
+    description: 'Forest spring leaves',
+  },
+  {
+    preview:
+      'https://pixabay.com/get/ge40bd361c4b019ae618f48723d4dfa48306b12addfe078c4fddd80c0867c3df4f990384eb15ded538e6fd92a8874073b_640.jpg',
+    original:
+      'https://pixabay.com/get/g3c28f4f1aa97a678946c70576db90dc1f306cfd3479b5472bd3b3835dc26f0c9ef7e98e2dd9d174632de816789a6cea0b8fdfb3a92ad25f31db93eb856898210_1280.jpg',
+    description: 'Surfer sea waves surf',
   },
 ];
